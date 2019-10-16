9973249fc374035a6a184472697feb6b057c86b2
James Towers <Meandmy10@outlook.com>
2019-10-16 19:40:58 +1100

    Added Registration Endpoint

---
+++ LocationAPI/EmailSender.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Identity.UI.Services;
+using MimeKit;
+using MimeKit.Text;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LocationAPI
+{
+    public class EmailSender : IEmailSender
+    {
+        public Task SendEmailAsync(string email, string subject, string message)
+        {
+            //this needs to be implimented at some point if we want to use email confirmation
+            return Task.CompletedTask;
+        }
+    }
+}
