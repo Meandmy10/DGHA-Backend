3c7a36f2454b8ec0b1375e45ab6352923d973dca
Lee Chu Yem <leechuyem@gmail.com>
2019-11-11 20:27:51 +1100

    edit search contoller

--- API/Controllers/SearchController.cs
+++ API/Controllers/SearchController.cs
@@ -1,52 +1,56 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
+using System.Linq;
 using Microsoft.AspNetCore.Mvc;
-using System.Net.Http;
-using Newtonsoft.Json;
+using Microsoft.EntityFrameworkCore;
+using ModelsLibrary;
+using ModelsLibrary.Data;
+
+// MODELS
 using API.Models;
 
 #pragma warning disable CS1591 // Missing XML comment for publicly visible type or member
 #pragma warning disable CA1031
+#pragma warning disable CA2007
 
-namespace API.Controllers
-{
+namespace API.Controllers {
     [ApiController]
-    [Route("search")]
-    public class SearchController : Controller
-    {
-        private string apiKey = "AIzaSyCj1t28RYFPFpa1xL_kjlIwCrH8CEryoJs";
+    [Route ("search")]
+    public class SearchController : Controller {
+        private readonly ApplicationDbContext _context;
+        public SearchController (ApplicationDbContext context) {
+            _context = context;
+        }
+
         [HttpGet]
-        public async IAsyncEnumerable<List<Place>> getSearch(string query)
-        {
-            string url = $"https://maps.googleapis.com/maps/api/place/textsearch/json?query={query}&key={apiKey}";
-            List<Place> places = new List<Place>();
-            try
-            {
-                using (HttpClient client = new HttpClient())
-                {
-                    using (HttpResponseMessage res = await client.GetAsync(url))
-                    {
-                        using (HttpContent content = res.Content)
-                        {
-                            var data = await content.ReadAsStringAsync().ConfigureAwait(true);
-                            var dataObj = JsonConvert.DeserializeObject<PlaceAPIQueryResponse>(data);
-                            var results = dataObj.results;
+        public async IAsyncEnumerable<Place> getSearch (string query) {
+            var results = await HttpReq.getPlaceByTextFromGoogle (query).ConfigureAwait (false);
 
-                            foreach (var item in results)
-                            {
-                                Place p = new Place(placeId: item.place_id, name: item.name, address: item.formatted_address, types: item.types);
-                                places.Add(p);
-                            }
-                        }
-                    }
+            foreach (var item in results) {
+                string placeId = item.place_id;
+
+                Place place = new Place ();
+                place.PlaceId = placeId;
+                place.Name = item.name;
+                place.Address = item.formatted_address;
+                place.Types = item.types;
+
+                // In this case, state doesn't matter
+                place.State = null;             
+
+                // get the reviews
+                List<Review> placeReviews = await _context.Reviews.Where (review => review.PlaceID == placeId).ToListAsync ().ConfigureAwait (true);
+
+                if (placeReviews.Count > 0) {
+                    place.numOfRatings = placeReviews.Count;
+                    place.avgOverallRating = (float) placeReviews.Average (review => review.OverallRating);
+                    place.avgCustomerRating = (float) placeReviews.Average (review => review.ServiceRating);
+                    place.avgLocationRating = (float) placeReviews.Average (review => review.LocationRating);
+                    place.avgAmentitiesRating = (float) placeReviews.Average (review => review.AmentitiesRating);
+
                 }
-            }
-            catch (Exception e)
-            {
-                Console.WriteLine("and i oop- " + e);
-            }
 
-            yield return places;
+                yield return place;
+            }
         }
     }
 }
