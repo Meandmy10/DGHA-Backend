d7293d2cd294678aab718f3a99bfb7a170b94d34
Lee Chu Yem <leechuyem@gmail.com>
2019-11-12 18:39:12 +1100

    change the name SearchReponse and add constructor

---
+++ API/Models/SearchAPIResponse.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections.Generic;
+using Microsoft.AspNetCore.Mvc;
+
+
+#pragma warning disable CS1591 // Missing XML comment for publicly visible type or member
+#pragma warning disable CA2227 // Collection properties should be read only
+
+namespace API.Models
+{
+    public class SearchResponse
+    {
+        public List<Place> results { get; set; }
+        public string nextPageToken {get; set;}
+
+        public SearchResponse() {
+            this.results = new List<Place>();
+        }
+    }
+}
